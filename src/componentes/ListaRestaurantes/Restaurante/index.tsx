2d37901755f68cb18b8129f7bb00dfd0065056cd
7eduardocf <eduardocfrota901@gmail.com>
2025-07-02 20:17:11 -0400

    Implementada a exibição de pratos de restaurantes

--- src/componentes/ListaRestaurantes/Restaurante/index.tsx
+++ src/componentes/ListaRestaurantes/Restaurante/index.tsx
@@ -1,21 +1,35 @@
+import { useEffect, useState } from 'react';
 import IRestaurante from '../../../interfaces/IRestaurante';
 import Prato from '../Prato';
 import estilos from './Restaurante.module.scss';
+import axios from 'axios';
+import IPrato from '../../../interfaces/IPrato';
 
 interface RestauranteProps {
-  restaurante: IRestaurante
+    restaurante: IRestaurante
 }
 
 const Restaurante = ({ restaurante }: RestauranteProps) => {
 
-  return (<section className={estilos.Restaurante}>
-    <div className={estilos.Titulo}>
-      <h2>{restaurante.nome}</h2>
-    </div>
-    <div>
-      {restaurante.pratos?.map(item => <Prato prato={item} key={item.id} />)}
-    </div>
-  </section>)
+    const [pratos, setPratos] = useState<IPrato[]>()
+
+    useEffect(()=>{
+        axios.get("http://localhost:8000/api/v1/pratos/")
+        .then(response =>{
+            setPratos(response.data.results)
+        })
+    },[restaurante.id])
+
+    return (<section className={estilos.Restaurante}>
+        <div className={estilos.Titulo}>
+            <h2>{restaurante.nome}</h2>
+        </div>
+        <div>
+            {pratos?.map(item =>{
+                return <Prato prato={item} key={item.id} />
+            })}
+        </div>
+    </section>)
 }
 
 export default Restaurante
