7e9cba78ed1c78db160c336533d96c125a0c12ca
7eduardocf <eduardocfrota901@gmail.com>
2025-07-17 00:18:22 -0400

    Ajustando layout da pagina para funcionalidade de criação de pratos

--- src/paginas/Administracao/Pratos/FormularioPrato.tsx
+++ src/paginas/Administracao/Pratos/FormularioPrato.tsx
@@ -1,11 +1,20 @@
-import { Box, Button, TextField, Typography } from "@mui/material"
-import { useState } from "react"
+import { Box, Button, FormControl, InputLabel, MenuItem, Select, TextField, Typography } from "@mui/material"
+import { useEffect, useState } from "react"
+import http from "../../../Http"
+import ITag from "../../../interfaces/ITag"
 
 
 const FormularioPrato = () => {
 
 
     const [nomeDoPrato, setNomeDoPrato] = useState("")
+    const [descricao, setDescricaoDoPrato] = useState("")
+    const [tags, setTags] = useState<ITag[]>([])
+
+    useEffect(()=>{
+        http.get< {tags: ITag[]} >("tags/")
+            .then(response => setTags(response.data.tags))
+    },[])
 
     const aoSubmeterForm = (evento: React.FormEvent<HTMLFormElement>) => {
         evento.preventDefault()
@@ -21,11 +30,29 @@ const FormularioPrato = () => {
                 <TextField
                     value={nomeDoPrato}
                     onChange={evento => setNomeDoPrato(evento.target.value)}
-                    label="Nome do restaurante"
+                    label="Nome do prato"
                     variant="standard"
                     fullWidth
                     required
+                    margin="dense"
                 />
+                <TextField
+                    value={descricao}
+                    onChange={evento => setDescricaoDoPrato(evento.target.value)}
+                    label="Descrição do prato"
+                    variant="standard"
+                    fullWidth
+                    required
+                    margin="dense"
+                />
+                <FormControl margin="dense" fullWidth required>
+                    <InputLabel id="select-tag">Tag</InputLabel>
+                    <Select labelId="select-tag" value={tag} onChange={evento => setTags(evento.target.value)}>
+                        {tags.map(tag => <MenuItem value={tag.id} key={tag.id}>
+                            {tag.value}
+                        </MenuItem>)}
+                    </Select>
+                </FormControl>
                 <Button sx={{ marginTop: 1 }} type="submit" fullWidth variant="outlined">Salvar</Button>
             </Box>
         </Box>
